0672dd66f5011ada11a2df674c6d16bd02b783df
Can Temizyurek <can@temizyurek.com>
2025-10-01 15:00:49 -0400

    feat: implement user authentication check in route loader to redirect unauthenticated users

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { getCurrentUser } from '@/lib/services/user'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/")({
+export const Route = createFileRoute('/')({
   component: App,
-});
+  loader: async () => {
+    const user = await getCurrentUser()
+    if (!user) {
+      throw redirect({ to: '/auth/sign-in' })
+    }
+    throw redirect({ to: '/chat' })
+  },
+})
 
 function App() {
-  return <div></div>;
+  return <div></div>
 }
